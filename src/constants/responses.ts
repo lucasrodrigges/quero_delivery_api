17bf51d31473deed561f2cf876eef0ead30f8ed2
lucasrodrigges <rodriggeslucas@gmail.com>
2025-02-04 01:24:11 -0300

    wip: init server

---
+++ src/constants/responses.ts
@@ -0,0 +1,38 @@
+export default {
+  OK: {
+    status: 200,
+    message: "OK"
+  },
+  CREATED: {
+    status: 201,
+    message: "Created"
+  },
+  CONFLICT: {
+    status: 409,
+    message: "Conflict"
+  },
+  BAD_REQUEST: {
+    status: 400,
+    message: "Bad Request"
+  },
+  UNAUTHORIZED: {
+    status: 401,
+    message: "Unauthorized"
+  },
+  FORBIDDEN: {
+    status: 403,
+    message: "Forbidden"
+  },
+  NOT_FOUND: {
+    status: 404,
+    message: "Not Found"
+  },
+  INVALID_VALUES: {
+    status: 422,
+    message: "Invalid Values"
+  },
+  INTERNAL_SERVER_ERROR: {
+    status: 500,
+    message: "Internal Server Error"
+  }
+}
